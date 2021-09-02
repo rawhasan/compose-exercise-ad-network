f412fe98b2a1944292b0088d2e4352dbc27e261c
rawhasan <rawhasanweb@gmail.com>
2021-09-02 20:08:17 +0600

    interstitial ad not showing

--- app/src/main/java/com/example/adnetwork/MainActivity.kt
+++ app/src/main/java/com/example/adnetwork/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.adnetwork
 
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,13 +20,15 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
 import com.example.adnetwork.ui.theme.AdNetworkTheme
 import com.google.android.gms.ads.*
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
+private var mInterstitialAd: InterstitialAd? = null
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -35,6 +42,47 @@ class MainActivity : ComponentActivity() {
 
         // initialize the Mobile Ads SDK
         MobileAds.initialize(this) { }
+
+        // load interstitial ad
+        InterstitialAd.load(
+            this,
+            getString(R.string.ad_id_interstitial),
+            AdRequest.Builder().build(),
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    mInterstitialAd = null
+                    Log.d("MainActivity", adError.message)
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    mInterstitialAd = interstitialAd
+                    Log.d("MainActivity", "Ad was loaded.")
+                }
+            }
+        )
+
+        mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+            override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+                Log.d("MainActivity", "Ad failed to show.")
+            }
+
+            override fun onAdShowedFullScreenContent() {
+                mInterstitialAd = null
+                Log.d("MainActivity", "Ad showed fullscreen content.")
+            }
+
+            override fun onAdDismissedFullScreenContent() {
+                Log.d("MainActivity", "Ad was dismissed.")
+            }
+        }
+
+        // show the interstitial ad
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+        } else {
+            Log.d("MainActivity", "The interstitial ad wasn't ready yet.")
+        }
+
     }
 }
 
@@ -46,6 +94,8 @@ fun AdNetworkApp() {
 
     val context = LocalContext.current
     val interstitial_ad_id = stringResource(id = R.string.ad_id_interstitial)
+
+    //val interestitialAd: InterstitialAd? = null
 
     Column(
         modifier = Modifier
@@ -105,22 +155,7 @@ fun AdNetworkApp() {
         // Interstitial ad on button click - Working
         // (showing success on log, but not displaying the ad
         Button(
-            onClick = {
-                InterstitialAd.load(
-                    context,
-                    interstitial_ad_id,
-                    AdRequest.Builder().build(),
-                    object : InterstitialAdLoadCallback() {
-                        override fun onAdFailedToLoad(adError: LoadAdError) {
-                            Log.d("MainActivity", adError.message)
-                        }
-
-                        override fun onAdLoaded(interstitialAd: InterstitialAd) {
-                            Log.d("MainActivity", "Ad was loaded.")
-                        }
-                    }
-                )
-            },
+            onClick = {  },
             modifier = Modifier.padding(16.dp)
         ) {
             Text(text = "Show Interstitial")
